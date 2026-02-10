529290c4320d4bf4a4f076cba883f3d11d68f7fa
Rodrigo Basniak <rodrigo.basniak@aveva.com>
2026-02-10 08:57:57 +0100

    checkpoint

---
+++ back/PaintingProjectsManagement.Features.Core.Tests/Usings.cs
@@ -0,0 +1,9 @@
+global using Shouldly;
+global using PaintingProjectsManagement.Api;
+global using Microsoft.Extensions.DependencyInjection;
+global using rbkApiModules.Commons.Testing;
+global using rbkApiModules.Identity.Core;
+global using System.Net;
+global using rbkApiModules.Testing.Core;
+global using Microsoft.EntityFrameworkCore;
+global using TUnit.Core;
--- back/PaintingProjectsManagement.Features.Materials.UI.Tests/Usings.cs
+++ back/PaintingProjectsManagement.Features.Materials.UI.Tests/Usings.cs
@@ -1,3 +1,4 @@
 global using Shouldly;
 global using TUnit.Core;
 global using Microsoft.Playwright;
+global using PaintingProjectsManagement.Testing.Core;
